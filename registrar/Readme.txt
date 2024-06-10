e50d65bc6c6c151bd9d644f7c27eb5d61cd1263f
unknown <aruizcal@misena.edu.co>
2024-06-09 22:39:55 -0500

    version api2

---
+++ registrar/Readme.txt
@@ -0,0 +1,27 @@
+Cómo ejecutar el registro de usuario e inicio de sesión y el sistema de gestión de usuarios con el proyecto del panel de administración:
+
+1. Download the  zip file
+
+2. Extract the file and copy loginsystem folder
+
+3.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+4. Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+5. Import loginsystem.sql file(given inside the zip package in SQL file folder)
+
+6.Run the script http://localhost/loginsystem (frontend)
+
+7. For admin Panel http://localhost/loginsystem/admin
+
+Credential for admin panel :
+
+Username: admin
+Password: admin
+
+Credential for user panel : 
+
+Username: user@recursivoweb.com
+Password : 123456
+
+You can also drop a mail at inforecursivoweb@gmail.com
---
+++ registrar/SQL File/loginsystem.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.4.22-MariaDB : Database - loginsystem
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`loginsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `loginsystem`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`id`,`username`,`password`) values (1,'admin','21232f297a57a5a743894a0e4a801fc3');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(300) DEFAULT NULL,
+  `contactno` varchar(11) DEFAULT NULL,
+  `posting_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`fname`,`lname`,`email`,`password`,`contactno`,`posting_date`) values (13,'Recursivoweb','Online','user@recursivoweb.com','123456','1234567890','2021-08-09 13:30:00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
